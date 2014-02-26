be6bbde5d00da6d83f762b7b400cc4f6bdf30a11
dhh1128 <dhardman@adaptivecomputing.com>
2014-02-25 20:59:58 -0700

    Make trim unit tests pass

--- src/core/strutil-inline.h
+++ src/core/strutil-inline.h
@@ -4,6 +4,9 @@
 #include <string.h>
 
 inline char const * ltrim(char const * begin, char const * end, char const * chars) {
+    if (begin == nullptr) return nullptr;
+    if (end < begin) return begin;
+    if (chars == nullptr) chars = ANY_WHITESPACE;
     while (begin < end) {
         if (strchr(chars, *begin) == nullptr) {
             return begin;
@@ -14,13 +17,15 @@ inline char const * ltrim(char const * begin, char const * end, char const * cha
 }
 
 inline char const * rtrim(char const * begin, char const * end, char const * chars) {
-    while (end > begin) {
+    if (begin == nullptr) return nullptr;
+    if (end < begin) return begin;
+    if (chars == nullptr) chars = ANY_WHITESPACE;
+    while (--end > begin) {
         if (strchr(chars, *end) == nullptr) {
-            return end;
+            return end + 1;
         }
-        --end;
     }
-    return end;
+    return begin;
 }
 
 #endif
--- test/core/strutil_test.cpp
+++ test/core/strutil_test.cpp
@@ -2,15 +2,23 @@
 
 #include "gtest/gtest.h"
 
-typedef char const * (*trim_func)(char const *, char const *, char const *);
+#define try_trim_ex(trimmer, cstr, expected, chars) \
+    { char const * end = strchr(cstr, 0); \
+    EXPECT_STREQ(expected, trimmer(cstr, end, chars)); }
 
-void try_trim(trim_func trimmer, char const * cstr, char const * expected, char const * chars=ANY_WHITESPACE) {
-    char const * end = strchr(cstr, 0);
-    EXPECT_STREQ(expected, trimmer(cstr, end, chars));
-}
+#define try_trim(trimmer, cstr, expected) \
+    try_trim_ex(trimmer, cstr, expected, ANY_WHITESPACE)
 
 TEST(strutil_test, ltrim) {
     try_trim(ltrim, " \ttest", "test");
     try_trim(ltrim, "test ", "test ");
+    try_trim_ex(ltrim, "test ", "st ", "et");
+    try_trim(ltrim, "\r\n \t ", "");
 }
 
+TEST(strutil_test, rtrim) {
+    try_trim(rtrim, "test \t", " \t");
+    try_trim(rtrim, " test", "");
+    try_trim_ex(rtrim, " test", "st", "st");
+    try_trim(rtrim, "\r\n \t ", "\r\n \t ");
+}
