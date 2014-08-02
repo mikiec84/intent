5cbbcbb4d6fe3307e9a3af2770ec7d786504649e
Daniel Hardman <daniel.hardman@gmail.com>
2014-08-01 22:01:18 -0600

    make scons use env variables

--- buildutil.py
+++ buildutil.py
@@ -50,16 +50,16 @@ warnings = ' '.join(w.strip() for w in warnings.strip().split('\n'))
 _flags = "-D_FILE_OFFSET_BITS=64 " + warnings + ' '
 
 def ccflags(release):
-	if release:
-		return _flags + '-O3'
-	return _flags + '-g'
+    if release:
+        return _flags + '-O3'
+    return _flags + '-g'
 
 _my_folder = os.path.dirname(os.path.abspath(__file__))
 def list_buildable_subdirs(folder):
-	vsd = os.listdir(folder)
-	vsd = [x for x in vsd if not x.startswith('.')]
-	vsd = [x for x in vsd if os.path.isfile(os.path.join(folder, x, 'sconscript'))]
-	return vsd
+    vsd = os.listdir(folder)
+    vsd = [x for x in vsd if not x.startswith('.')]
+    vsd = [x for x in vsd if os.path.isfile(os.path.join(folder, x, 'sconscript'))]
+    return vsd
 
 external_projects = list_buildable_subdirs(os.path.join(_my_folder, 'src', 'external'))
 components = list_buildable_subdirs(os.path.join(_my_folder, 'src'))
@@ -69,22 +69,22 @@ components.append('i')
 tests = list_buildable_subdirs(os.path.join(_my_folder, 'test'))
 
 def check_config(config):
-	# Do some very simple sanity checking on tools. Prove that we have a compiler
-	# that's able to deal with C++ 11 constructs (typically, g++ 4.6 or later).
+    # Do some very simple sanity checking on tools. Prove that we have a compiler
+    # that's able to deal with C++ 11 constructs (typically, g++ 4.6 or later).
     #    if not config.CheckCXX():
     #            print('C++ compiler unavailable.')
     #            sys.exit(1)
 
-	required_headers = ['unordered_map', 'cstdint']
-	missing = []
-	for rh in required_headers:
-		if not config.CheckHeader(rh, language='C++'):
-			missing.append(rh)
-	if missing:
-		print('The following headers are unavailable, but required to compile: %s' %
-			', '.join(['<%s>' % h for h in missing]))
-        f = open('config.log', 'r')
-        txt = f.read()
-        f.close()
-        print(txt)
-        sys.exit(1)
+    required_headers = ['unordered_map', 'cstdint']
+    missing = []
+    for rh in required_headers:
+        if not config.CheckHeader(rh, language='C++'):
+            missing.append(rh)
+    if missing:
+        print('The following headers are unavailable, but required to compile: %s' %
+              ', '.join(['<%s>' % h for h in missing]))
+    f = open('config.log', 'r')
+    txt = f.read()
+    f.close()
+    print(txt)
+    sys.exit(1)
