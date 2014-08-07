da3aecdea5cda2f32be303bebb951a283f6a281c
Daniel Hardman <daniel.hardman@gmail.com>
2014-08-06 23:51:39 -0600

    add a script to detect which clang sanitizers are available

---
+++ cmake/detect_sanitizers.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import os, sys, subprocess, shlex, tempfile
+
+def detect_available_sanitizers(compiler):
+    possible = 'integer,undefined,unsigned-integer-overflow,address,thread,mem,dataflow'.split(',')
+    folder = '/tmp' #tempfile.mkdtemp()
+    available = []
+    try:
+        cpp = '''int main() { return 0; }'''
+        cpp_path = os.path.join(folder, 'x.cpp') 
+        with open(cpp_path, 'w') as f:
+            f.write(cpp)
+        try:
+            for candidate in possible:
+                try:
+                    subprocess.check_output([compiler, '-fsanitize=%s' % candidate, cpp_path], stderr=subprocess.STDOUT)
+                    available.append(candidate)
+                except subprocess.CalledProcessError:
+                    pass
+        finally:
+            pass #os.remove(cpp_path)
+        print(' '.join(available))
+    finally:
+        pass #os.rmdir(folder)
+
+if __name__ == '__main__':
+    compiler = 'clang++'
+    if len(sys.argv) == 2:
+        compiler = sys.argv[1]
+    detect_available_sanitizers(compiler)
+
