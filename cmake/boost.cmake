552156e2e47f480c06951722b7aabe81884bb357
Daniel Hardman <daniel.hardman@gmail.com>
2014-08-12 21:16:42 -0600

    Fix problem where memory sanitizer build wasn't linking correctly due to incorrect propagation of Boost_USE_STATIC_LIBS

--- cmake/boost.cmake
+++ cmake/boost.cmake
@@ -1,8 +1,14 @@
 # See http://j.mp/1qS8GbQ and http://j.mp/1uV5U7A for info about how to use
 # boost with cmake.
-if ("${Boost_USE_STATIC_LIBS}" STREQUAL "")
+if (${CMAKE_BUILD_TYPE} MATCHES "sanitize-(thread|memory)")
+    set(Boost_USE_STATIC_LIBS OFF)
+    message("Using dynamic libs for boost for compatibility with sanitizers.")
+else()
     set(Boost_USE_STATIC_LIBS ON)
 endif()
+# This setting should permeate the build everywhere; put it in the cache and don't
+# allow it to be overridden.
+set(Boost_USE_STATIC_LIBS ${Boost_USE_STATIC_LIBS} CACHE BOOL "link against static libs for boost" FORCE)
 set(Boost_USE_MULTITHREADED ON)
 set(Boost_USE_STATIC_RUNTIME ON)
 find_package(Boost 1.48.0 REQUIRED COMPONENTS filesystem system)
